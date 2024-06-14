9279e483a32e92262438c6d38ca5bb15a6632a49
JoaoCarlosNogueira <joaocarlos2002_br@hotmail.com>
2024-06-14 14:20:57 -0400

    ajustes

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,7 @@ export class FormBuscaService {
  formBusca: FormGroup;
   constructor() { 
     this.formBusca = new FormGroup({
-      
+      somenteIda: new FormControl(false)
     })
   }
 }
