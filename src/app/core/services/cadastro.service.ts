6a10e87172e0cb4d0a3a9224242a723bd6b8bf5c
JoaoCarlosNogueira <joaocarlos2002_br@hotmail.com>
2024-11-25 10:48:46 -0400

    criado o metodo buscarCadastro e editarCadastro

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { PessoaUsuario } from '../types/type';
@@ -17,6 +17,20 @@ export class CadastroService {
 
    cadastrar(pessoa: PessoaUsuario): Observable<PessoaUsuario>{
     return this.httpClient.post<PessoaUsuario>(`${this.apiUrl}/auth/cadastro`,pessoa)
+   } 
+
+   Buscarcadastro(token: string): Observable<PessoaUsuario>{
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    return this.httpClient.get<PessoaUsuario>(`${this.apiUrl}/auth/perfil`,{headers})
+   }
+   
+   Editarcadastro( pessoaUsuario: PessoaUsuario, token: string): Observable<PessoaUsuario>{
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    return this.httpClient.patch<PessoaUsuario>(`${this.apiUrl}/auth/perfil`, pessoaUsuario, {headers})
    }
 
 }
